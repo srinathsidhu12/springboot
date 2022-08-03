2f1c922e999230e7e7425c7dabf8d6256970df45
ashutoshsingh71 <ashutoshbit71@gmail.com>
2022-08-04 01:29:29 +0530

    spring boot application deployment to ec2 instance

---
+++ src/main/java/com/ashu/aws/example/api/SpringBootAwsEc2ExampleApplication.java
@@ -0,0 +1,21 @@
+package com.ashu.aws.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAwsEc2ExampleApplication {
+
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to first aws ec2 deployment";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsEc2ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashu/aws/example/api/SpringBootAwsEc2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.aws.example.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsEc2ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
